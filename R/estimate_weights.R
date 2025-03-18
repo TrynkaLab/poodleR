d82bb51a42c1fdc4c1734565af604c35bf33c065
Marta <ma23@sanger.ac.uk>
2025-03-18 16:16:05 +0000

    modified main function to allow extremely low number of SNPs
    Not ideal but possible

--- R/estimate_weights.R
+++ R/estimate_weights.R
@@ -13,6 +13,106 @@ is_identical_genotype <- function(x, ...) {
   (rowSums((x - rowMeans(x, ...))^2, ...)/(dim(x)[2] - 1)) == 0
 }
 
+#' Modify main least square regression function to allow for approximations
+#' for this error case:
+#' >quadprog::solve.QP(Dmat, dvec, t(Amat), bvec, meq = meq) : 
+#' "matrix D in quadratic function is not positive definite!"
+#' This can happen when the matrix D is not positive definite, which is a requirement for the quadprog package,
+#' and which may be caused by having fewer variants than donors.
+#' That is not ideal, but this approximation will allow you to use this package for those extreme cases.
+#' @param C a numeric vector of length n  (n = number of donors)
+#' @param d a numeric vector of length n
+#' @param A a numeric matrix of size m x n (m = number of variants, n = number of donors)
+#' @param b a numeric vector of length m
+#' @param Aeq a numeric matrix of size 1 x n
+#' @param beq a numeric vector of length 1
+#' @param lb a numeric vector of length n
+#' @param ub a numeric vector of length n
+#'
+#' @return a numeric vector of length n
+#' @export
+mod_lsqlincon = function (C, d, A = NULL, b = NULL, Aeq = NULL, beq = NULL, 
+          lb = NULL, ub = NULL) 
+{
+  if (!requireNamespace("quadprog", quietly = TRUE)) {
+    stop("quadprog needed for this function to work. Please install it.", 
+         call. = FALSE)
+  }
+  stopifnot(is.numeric(C), is.numeric(d))
+  if (is.null(A) && !is.null(b) || !is.null(A) && is.null(b)) 
+    stop("If any, both 'A' and 'b' must be NULL.")
+  if (is.null(Aeq) && !is.null(beq) || !is.null(Aeq) && is.null(beq)) 
+    stop("If any, both 'Aeq' and 'beq' must be NULL.")
+  if (!is.matrix(C)) 
+    C <- matrix(C, 1)
+  mc <- nrow(C)
+  nc <- ncol(C)
+  n <- nc
+  if (length(d) != mc) 
+    stop("Dimensions of 'C' and 'd' do not fit.")
+  if (is.null(A) && is.null(Aeq) && is.null(lb) && is.null(ub)) 
+    return(qr.solve(C, d))
+  if (!is.null(A)) {
+    if (!is.matrix(A)) 
+      A <- matrix(A, 1)
+    ma <- nrow(A)
+    na <- ncol(A)
+    if (na != n) 
+      stop("Number of columns of 'A' does not fit with 'C'.")
+    A <- -A
+    b <- -b
+  }
+  if (is.null(Aeq)) {
+    meq <- 0
+  }
+  else {
+    if (!is.matrix(Aeq)) 
+      Aeq <- matrix(Aeq, 1)
+    meq <- nrow(Aeq)
+    neq <- ncol(Aeq)
+    if (neq != n) 
+      stop("Number of columns of 'Aeq' does not fit with 'C'.")
+  }
+  if (is.null(lb)) {
+    diag_lb <- NULL
+  }
+  else {
+    if (length(lb) == 1) {
+      lb <- rep(lb, n)
+    }
+    else if (length(lb) != n) {
+      stop("Length of 'lb' and dimensions of C do not fit.")
+    }
+    diag_lb <- diag(n)
+  }
+  if (is.null(ub)) {
+    diag_ub <- NULL
+  }
+  else {
+    if (length(ub) == 1) {
+      ub <- rep(ub, n)
+    }
+    else if (length(ub) != n) {
+      stop("Length of 'ub' and dimensions of C do not fit.")
+    }
+    diag_ub <- -diag(n)
+    ub <- -ub
+  }
+  Dmat <- t(C) %*% C
+  
+  if(corpcor::is.positive.definite(as.matrix(Dmat))==FALSE){
+    message("matrix D in quadratic function is not positive definite!")
+    message("Computing positive definite matrix approximation ...")
+    Dmat = Matrix::nearPD(as.matrix(Dmat))$mat
+  }
+  dvec <- t(C) %*% d
+  Amat <- rbind(Aeq, A, diag_lb, diag_ub)
+  bvec <- c(beq, b, lb, ub)
+  rslt <- quadprog::solve.QP(Dmat, dvec, t(Amat), bvec, meq = meq)
+  rslt$solution
+}
+
+
 #' Estimate donor proportions (weights)
 #'
 #' Estimate donor proportions (weights) from the estimated MAF and the genotype
@@ -58,11 +158,12 @@ estimate_weights <- function(b, A=gt, force_zero = TRUE){
   #  Aeqw=beq  where Aeq  is a 1×k matrix of ones and be is a length m -vector, also of ones, contrains the sum of weights to 1
   #  lb=0 and ub=1  constraints our solution to wi∈[0,1]
 
-  w=pracma::lsqlincon(C=as.matrix(A[,..cols]),
+  w=mod_lsqlincon(C=as.matrix(A[,..cols]),
               d=b,
               Aeq=matrix(1,ncol=length(cols),nrow=1),
               beq=1,
-              lb=0,ub=1)
+              lb=0,
+              ub=1)
   if(force_zero == TRUE ) {
     w[w < 0] = 0
   }
